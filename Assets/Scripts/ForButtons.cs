af406c46b9f4c3d8d03544e7f855f1d31e03e161
Lizer-flesh <60391056+Lizer-flesh@users.noreply.github.com>
2022-02-04 18:05:42 +0300

    Update ForButtons.cs

--- Assets/Scripts/ForButtons.cs
+++ Assets/Scripts/ForButtons.cs
@@ -33,6 +33,8 @@ public class ForButtons : MonoBehaviour, IPointerClickHandler, IPointerDownHandl
 
     public void OneStepButton()
     {
-        public 
-    }
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            
+        }
 }
