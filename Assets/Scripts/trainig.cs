cd52f48a0405de24924247536d007a13b8259510
LizaLizer <lizer2002flesh@gmail.com>
2022-05-14 23:01:52 +0500

    Add lols

--- Assets/Scripts/trainig.cs
+++ Assets/Scripts/trainig.cs
@@ -4,33 +4,17 @@ using UnityEngine;
 
 public class trainig : MonoBehaviour
 {
-    public GameObject _backButton;
-    public GameObject _exitButton;
-    public GameObject _headText;
-    public GameObject _saveText;
-    public GameObject _list;
-    public GameObject _canText;
-    public GameObject _vosklImage;
-    public GameObject _vosklImage2;
-    public GameObject _strelka;
-    public GameObject _ramka;
-    public GameObject _ramka2;
-    public GameObject _nextButton;
+    public GameObject _panelStart;
+    public GameObject _step1Panel;
+    public GameObject _step2Panel;
+    
+   
 
      void Start()
     {
-        var _backButton = GameObject.Find("BackButton");
-        var _exitButton = GameObject.Find("ExitButton");
-        var _headText = GameObject.Find("HeadText");
-        var _saveText = GameObject.Find("SaveText");
-        var _list = GameObject.Find("List");
-        var _canText = GameObject.Find("CanText");
-        var  _vosklImage = GameObject.Find("VosklImage");
-        var _vosklImage2 = GameObject.Find("VosklImage2");
-        var _strelka = GameObject.Find("Strelka");
-        var  _ramka = GameObject.Find("Ramka");
-        var _ramka2 = GameObject.Find("Ramka2");
-        var _nextButton = GameObject.Find("NextButton");
+        var _panelStart = GameObject.Find("PanelStart");
+        var _step1Panel = GameObject.Find("Step1Panel");
+        var _step2Panel = GameObject.Find("Step2Panel");
 
         // _nextButton.onClick.AddListener(NextButtonOnClick());
     }
